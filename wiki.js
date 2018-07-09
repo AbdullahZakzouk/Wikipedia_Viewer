161db09322c9df736865f434559b7b870e285cfa
Abdullah Zakzouk <31353450+AbdullahZakzouk@users.noreply.github.com>
2018-07-09 21:27:25 +0200

    Create wiki.js

---
+++ wiki.js
@@ -0,0 +1,28 @@
+$("#searchform").on("submit", function(event) {
+  event.preventDefault();
+  var query = $("#searchquery").val();
+  fetch(query)
+    .done(function(data) {
+      console.log(data);
+      render(data.query.search);
+    });
+});
+function render(results) {
+  var $out = $("out");
+  var html = results.map(function(result) {
+    return '<a class="result"' +
+           '   href="https://en.wikipedia.org/wiki/'+result.title+'">' +
+           '  <h2>'+result.title+'</h2>' +
+           '  <div>'+result.snippet+'</div>' +
+           '</a><hr>';
+  }).join("\n\n");
+  $out.html("");
+  $(html).appendTo($out);
+}
+function fetch(query) {
+  return $.ajax({
+    url: '//en.wikipedia.org/w/api.php',
+    data: { action: 'query', list: 'search', srsearch: query, format: 'json' },
+    dataType: 'jsonp',
+  });
+}
